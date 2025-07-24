5c604d5c10e99cc0ebf49cc6ce2d51e45ad5cbca
Omarben05 <your-email@example.com>
2025-07-24 13:39:32 +0200

    add

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from flask import Flask, jsonify 
 # Import CORS to enable cross-origin requests (from different domains)
 from flask_cors import CORS
+# Import os and socket for potential use in the application (e.g., environment variables, networking)
+import os
+import socket
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -13,6 +16,15 @@ CORS(app)
 def hello():
     # Return a JSON response with a greeting message
     return jsonify(message="Ciao da k8s!")
+
+@app.route('/hostname')
+def get_hostname():
+    msg = os.getenv('MESSAGE', 'hello from k8s')
+    hostname= socket.gethostname()
+    # Get the hostname of the machine running the application
+    # Return a JSON response with the hostname and message
+    return jsonify(hostname=hostname, message=msg)
+
 
 # Check if the script is being run directly (not imported as a module)
 if __name__ == '__main__':
